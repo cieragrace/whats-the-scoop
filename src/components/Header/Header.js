a7fae8f9cc7347bda75e818efcae82cce6bbe8c4
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-11 16:16:55 -0600

    Add built out Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,16 @@
 import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <h1 className='whats-the-scoop'>What's the Scoop?</h1>
+      <section className='header-mid'>
+        <p className='extra-extra'>Extra! Extra!</p>
+        <p className='date'>This is the date</p>
+      </section>
+      <h5 className='todays-news'>Today's News at Your Finger Tips</h5>
+    </div>
+  )
+}
+
+export default Header
