f9a6f897b45a1c9f0941d41e6f480b32604ff3aa
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-12 07:58:40 -0600

    Add categoryAPI function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,28 @@
 import './App.css'
 import Home from '../Home/Home'
+import getAPIData from '../../apiCalls'
+import CategoryPage from '../CategoryCard/CategoryCard'
 import { Switch, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 const App = () => {
+const [storyList, setStoryList] = useState([])
 const categories = ["Arts", "Automobiles", "Books", "Business", "Fashion", "Food", 
                   "Health", "Home", "Insider", "Magazine", "Movies", "NY Region", 
                   "Obituaries", "Opinion", "Politics", "Real Estate", "Science", 
                   "Sports", "Sunday Review", "Technology", "Theater", "T-Magazine", 
                   "Travel", "Upshot", "US", "World"]
+
+const categoryAPICall = (category) => {
+  getAPIData(category)
+  .then((data) => {
+    setStoryList(data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
+
   return (
     <main className='app-container'>
       <Switch>
@@ -16,9 +31,18 @@ const categories = ["Arts", "Automobiles", "Books", "Business", "Fashion", "Food
           path="/"
           render={() => (
             <Home 
-              categories={categories} 
+              categories={categories}
+              categoryAPICall={categoryAPICall} 
             />
           )}
+        />
+        <Route 
+          exact
+          path="/:category"
+          render={() => (
+            <CategoryPage 
+            />
+          )}  
         />
       </Switch>
     </main>
