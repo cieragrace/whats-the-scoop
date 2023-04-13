d35378fbc4c276e1fa06cf8253c7d9d23fa576c5
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-13 14:17:54 -0600

    Add byline

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -6,6 +6,7 @@ const ArticleCard = ({story}) => {
 return (
   <a href={`${story.url}`} target="_blank" rel="noopener noreferrer" className='article-card-link'>
     <h2 className='story-title'>{`${story.title}`}</h2>
+    <p className='byline'>{`${story.byline}`}</p>
     <p className='abstract-container'>{`${story.abstract}`}</p>
   </a>
   )
