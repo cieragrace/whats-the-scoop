bec840c05413b8609770588962f97c53e045abea
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-13 14:49:29 -0600

    remove unused text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,6 @@ const Header = () => {
       <div className='header-text-container'>
         <h1 className='whats-the-scoop'>What's the Scoop?</h1>
         <div className='header-mid'>
-          {/* <p className='date'>This is the date</p> */}
-          {/* <p className='extra-extra'>Extra! Extra!</p> */}
         </div>
         <h5 className='todays-news'>Today's News at Your Finger Tips</h5>
       </div>
