da6fe985de187ad096225819df6a16d1c3a84ded
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-13 07:38:20 -0600

    Add single story API call frame

--- src/components/CategoryPage/CategoryPage.js
+++ src/components/CategoryPage/CategoryPage.js
@@ -3,6 +3,18 @@ import Header from '../Header/Header'
 import ArticleCard from '../ArticleCard/ArticleCard'
 
 const CategoryPage = ({stories}) => {
+// const [singleStory, setSingleStory] = useState({})
+
+// showStoryDetails = (id) => {
+//   const findStory = stories.find(story => story.id === id)
+//   getStoryAPICall(`stories/${story.id}`)
+//   .then((data) => {
+//     console.log('data', data)
+//     setSingleStory(data)
+//     console.log(singleStory)
+//     })
+//     .catch((error) => console.log(error))
+//   }
 
 const storyCard = stories.map((story, index) => {
   return(
