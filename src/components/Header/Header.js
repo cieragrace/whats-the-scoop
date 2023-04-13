17d043081ad920806416b2aba60e66c500ea9a68
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-13 12:33:10 -0600

    Add styling and images to home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,16 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header-container'>
-      <h1 className='whats-the-scoop'>What's the Scoop?</h1>
-      <div className='header-mid'>
-        <p className='date'>This is the date</p>
-        <p className='extra-extra'>Extra! Extra!</p>
+      <div className='logo-container'></div>
+      <div className='header-text-container'>
+        <h1 className='whats-the-scoop'>What's the Scoop?</h1>
+        <div className='header-mid'>
+          {/* <p className='date'>This is the date</p> */}
+          {/* <p className='extra-extra'>Extra! Extra!</p> */}
+        </div>
+        <h5 className='todays-news'>Today's News at Your Finger Tips</h5>
       </div>
-      <h5 className='todays-news'>Today's News at Your Finger Tips</h5>
+      <div className='logo-container-2'></div>
     </div>
   )
 }
