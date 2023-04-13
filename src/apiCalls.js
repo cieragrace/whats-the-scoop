5cc52047f23d32f3c59ee8e21bcf85b0fd416bdc
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-13 07:44:44 -0600

    Add hardcode to API to test data pull

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 
-function getAPIData(category) {
-  return fetch (`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=BbLtA2ebhIHX1hgePOP0CJGLPAV0CkhL`)
+function getAPIData() {
+  return fetch ('https://api.nytimes.com/svc/topstories/v2/travel.json?api-key=BbLtA2ebhIHX1hgePOP0CJGLPAV0CkhL')
   .then((res) => res.json())
 }
 
