bd1a1e1cc543295a5de3a12ade27ec8e0e96f5ee
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-14 08:17:06 -0600

    Update logo to Link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header-container'>
       <div className='logo-container'></div>
       <div className='header-text-container'>
-        <h1 className='whats-the-scoop'>What's the Scoop?</h1>
+        <Link to='/' className='whats-the-scoop'>What's the Scoop?</Link>
         <h5 className='todays-news'>Today's News at Your Finger Tips</h5>
       </div>
       <div className='logo-container-2'></div>
